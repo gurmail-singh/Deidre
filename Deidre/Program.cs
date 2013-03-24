bc07746957b8ea2266ee9c0abff80161509d5a2b
Gurmail Singh <gurmail.s@gmail.com>
2013-03-24 19:29:59 +0000

    with prn code

--- Deidre/Program.cs
+++ Deidre/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Deidre
 {
@@ -20,20 +21,44 @@ namespace Deidre
             dateTimeStr.ToString("YYYYMMDD");
             Console.WriteLine(dateTimeStr);
             Console.WriteLine(dateTimeStr.ToString("yyyyMMdd"));
-            string inputFile = @"\schedule.txt";
+            string inputFile = @"\schedule2.txt";
             StreamReader sr = new StreamReader(inputFile);
             string strLine = "";
+            //string sPattern =@"(?i)(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY){1}\s\d{1,2}{1}(ND|TH|RD){1}\s(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER){1}";
+            //string sPattern = @"(?i)(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY)";
+            string sPattern = @"(?i)(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY){1}\s(\d{1,2}){1}(ND|TH|RD){1}\s(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER){1}";
             while ((strLine = sr.ReadLine()) != null)
             {
-                Console.WriteLine(strLine);
+                Match match = Regex.Match(strLine, sPattern);
+                if (match.Success)                
+                //Console.WriteLine(strLine);
+                //if (System.Text.RegularExpressions.Regex.IsMatch(strLine, sPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                {
+                    System.Console.WriteLine("Found" + strLine);
+                    System.Console.WriteLine("match found {0}'", match.Value);
+                    //1Will  help you?			09067 577 162	S20
+                    string prnPattern = @"(?i)(\d{5})\(sd{3})\s(d{3})\s(wd{2})";
+                    for(int i=0; i<5; i++) 
+                    {
+                        strLine = sr.ReadLine();
+                        Match match = Regex.Match(strLine, prnPattern);
+                        if (match.Success)
+                        {
+                        string prnVlaue = 
+                        }
+                    }
+                
 
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine("NOT Found" + strLine);
+                }
             }
             //(?i)(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY){1}\s\d{1,2}{1}(ND|TH|RD){1}\s(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER){1}
             //Cleanup
             sr.Close();
-
-            *
-
         }  
     }
 }
